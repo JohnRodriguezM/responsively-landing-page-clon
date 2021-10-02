c26f416c382214128ee56139e291efccf68e1d9e
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-10-02 17:10:55 -0500

    inicio proyecto, agregadas fuentes, link rel font awesome, script scrol reveal, falta reunir imagenes, maquetación header inicial, display none para div 2 y 3 header

---
+++ js/animaciones.js
@@ -0,0 +1,52 @@
+window.sr = ScrollReveal();
+
+    //sr.reveal('.',{
+       // rotate:{
+         //   x: 360,
+         //   y:180
+         //       }
+    //});
+/*
+    sr.reveal('.hola',{
+        duration:50000,
+        origin:'left',
+        distance:'-200px'
+    });
+    /*
+    sr.reveal('.',{
+        duration:12000,
+        origin:'top',
+        distance:'-100px'
+    });
+    sr.reveal('.',{
+        duration:7500,
+        //origin:'left',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.',{
+        duration:3500,
+        //origin:'right',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.',{
+        duration:3500,
+        //origin:'right',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.',{
+        duration:3500,
+        //origin:'left',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.',{
+        duration:3500,
+        //origin:'right',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('#',{
+        duration:4000,
+        rotate:{
+            x: 360,
+            y:180
+                }
+    });*/
