38b6dc662c34ae19cc8e72dba18ff82334f5763b
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-10-04 20:20:26 -0500

    cuadrando detalles

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,48 +1,77 @@
 window.sr = ScrollReveal();
 
-    //sr.reveal('.',{
-       // rotate:{
-         //   x: 360,
-         //   y:180
-         //       }
-    //});
-/*
-    sr.reveal('.hola',{
-        duration:50000,
-        origin:'left',
+    sr.reveal('.img_header',{
+        rotate:{
+            x: 360,
+            y:180
+                }
+    });
+
+    /*sr.reveal('.screenshot1',{
+        duration:1000,
+        origin:'top',
+    });*/
+
+    sr.reveal('.main_section2-interna-div1',{
+        duration:2000,
+        origin:'top',
         distance:'-200px'
     });
-    /*
-    sr.reveal('.',{
-        duration:12000,
+    sr.reveal('.main_section2-interna-div2',{
+        duration:2200,
         origin:'top',
-        distance:'-100px'
+        distance:'-200px'
     });
-    sr.reveal('.',{
-        duration:7500,
-        //origin:'left',//quitar
-        //distance:'-100px'//quitar
+    sr.reveal('.main_section2-interna-div3',{
+        duration:2400,
+        origin:'top',
+        distance:'-200px'
     });
-    sr.reveal('.',{
-        duration:3500,
-        //origin:'right',//quitar
-        //distance:'-100px'//quitar
+    sr.reveal('.video1',{
+        duration:2500,
+        origin:'top',
+        distance:'-50px'
     });
-    sr.reveal('.',{
-        duration:3500,
-        //origin:'right',//quitar
-        //distance:'-100px'//quitar
+    sr.reveal('.video2',{
+        duration:2500,
+        origin:'bottom',
+        distance:'50px'
     });
-    sr.reveal('.',{
-        duration:3500,
-        //origin:'left',//quitar
-        //distance:'-100px'//quitar
+    sr.reveal('.video3',{
+        duration:2500,
+        origin:'top',
+        distance:'-50px'
     });
-    sr.reveal('.',{
-        duration:3500,
-        //origin:'right',//quitar
-        //distance:'-100px'//quitar
+    sr.reveal('.video4',{
+        duration:2500,
+        origin:'bottom',
+        distance:'50px'
     });
+
+
+
+    sr.reveal('.main_section4-texto1',{
+        duration:2500,
+        origin:'top',
+        distance:'-50px'
+    });
+    sr.reveal('.main_section4-texto2',{
+        duration:2500,
+        origin:'bottom',
+        distance:'50px'
+    });
+    sr.reveal('.main_section4-texto3',{
+        duration:2500,
+        origin:'top',
+        distance:'-50px'
+    });
+    sr.reveal('.main_section4-texto4',{
+        duration:2500,
+        origin:'bottom',
+        distance:'50px'
+    });
+
+/*
     sr.reveal('#',{
         duration:4000,
         rotate:{
